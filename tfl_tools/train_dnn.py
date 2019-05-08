c23e6cdaddbeb00784c33d512ecbfd79455cf0fa
ayas <ayas@aucegypt.edu>
2019-05-08 19:26:11 +0200

    Train DNN without Cross Validation

--- tfl_tools/train_dnn.py
+++ tfl_tools/train_dnn.py
@@ -10,11 +10,12 @@ from net import Net
 # -- PATHS ---------------------------
 # DATABASE_PATH = 'Z:/DATA/dataset_test'
 # MODEL_PATH = 'Z:/DATA/model/modelCV2'
-DATABASE_PATH = '/mnt/DATA/silcam_classification_database'
-MODEL_PATH = '/mnt/DATA/model/modelORGNET'
+#DATABASE_PATH = '/mnt/DATA/silcam_classification_database'
+DATABASE_PATH = '/mnt/DATA/dataset'
+MODEL_PATH = '/mnt/DATA/model/modelVGGNet'
 #DATABASE_PATH = 'Z:/DATA/dataset'
 #MODEL_PATH = 'Z:/DATA/model/modelORGNET'
-LOG_FILE = os.path.join(MODEL_PATH, 'OrgNetDNNGPUSmall.log')
+LOG_FILE = os.path.join(MODEL_PATH, 'VGGNetDNNGPU.log')
 HEADER_FILE = os.path.join(MODEL_PATH, "header.tfl.txt")         # the header file that contains the list of classes
 set_file = os.path.join(DATABASE_PATH,"image_set.dat")     # the file that contains the list of images of the testing dataset along with their classes
 # set_file = os.path.join(MODEL_PATH,"image_set_win.dat")     # the file that contains the list of images of the testing dataset along with their classes
@@ -23,15 +24,17 @@ IMXY = 32
 # -----------------------------
 SPLIT_PERCENT = 0.05   # split the train and test data i.e 0.05 is a 5% for the testing dataset and 95% for the training dataset
 
-name='OrgNet'
-input_width=32
-input_height=32
+name='VGGNet'
+input_width=224
+input_height=224
 input_channels=3
 num_classes=7
 
-learning_rate=0.001  # 0.001 for OrgNet -- 0.01 for MINST -- 0.001 for CIFAR10 --
+learning_rate=0.0001  # 0.001 for OrgNet -- 0.01 for MINST -- 0.001 for CIFAR10 -- 0.001 for AlexNet
+                        # 0.0001 for VGGNet
 momentum=0.9
-keep_prob=0.75  # 0.75 for OrgNet -- 0.8 for LeNet -- 0.5 for CIFAR10
+keep_prob=0.5  # 0.75 for OrgNet -- 0.8 for LeNet -- 0.5 for CIFAR10 -- 0.5 for AlexNet
+                # 0.5 for VGGNET
 
 n_epoch = 50  # 50
 batch_size = 128 # 128
@@ -50,7 +53,7 @@ tf.reset_default_graph()
 print("trainY: ", trainY)
 print("testY: ", testY)
 
-model_file = os.path.join(MODEL_PATH, name +'GPUSMALL/plankton-classifier.tfl')
+model_file = os.path.join(MODEL_PATH, name +'GPU/plankton-classifier.tfl')
 model, conv_arr = LeNet.build_model(model_file)
 
 # Training
