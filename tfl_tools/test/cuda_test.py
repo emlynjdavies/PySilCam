4a712249b8c60e640b99f1604d32d5b4d8e9847f
ayas <ayas@aucegypt.edu>
2019-05-20 10:34:51 +0200

    Train PlanktonNet without Cross Validation

--- tfl_tools/test/cuda_test.py
+++ tfl_tools/test/cuda_test.py
@@ -1,3 +1,26 @@
 from numba import cuda
+import numpy
+
+@cuda.jit
+def my_kernel(io_array):
+    """
+    Code for kernel.
+    """
+    # code here
 
 print(cuda.gpus)
+
+# Create the data array - usually initialized some other way
+data = numpy.ones(256)
+
+# Set the number of threads in a block
+threadsperblock = 32
+
+# Calculate the number of thread blocks in the grid
+blockspergrid = (data.size + (threadsperblock - 1)) // threadsperblock
+
+# Now start the kernel
+my_kernel[blockspergrid, threadsperblock](data)
+
+# Print the result
+print(data)
