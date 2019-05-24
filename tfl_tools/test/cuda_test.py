b80d5bdfa17dad8dd4ed193f8fcb88fb23fc31a5
ayas <ayas@aucegypt.edu>
2019-05-24 10:21:40 +0200

    Train PlanktonNet without Cross Validation

--- tfl_tools/test/cuda_test.py
+++ tfl_tools/test/cuda_test.py
@@ -1,6 +1,7 @@
 # created a cudatest environment on GPU for testing
 from numba import cuda
 import numpy
+import math
 
 @cuda.jit
 def my_kernel(io_array):
@@ -18,6 +19,15 @@ def my_kernel(io_array):
     if pos < io_array.size:  # Check array boundaries
         io_array[pos] *= 2  # do the computation
 
+def my_kernel_2D(io_array):
+    x, y = cuda.grid(2)
+    ### YOUR SOLUTION HERE
+    tx = cuda.threadIdx.x
+    ty = cuda.blockIdx.y
+    bwx = cuda.blockDim.x
+    bwy = cuda.blockDim.y
+    io_array[tx,ty] *= 2  # do the computation
+
 print(cuda.gpus)
 
 # Create the data array - usually initialized some other way
@@ -33,4 +43,12 @@ blockspergrid = (data.size + (threadsperblock - 1)) // threadsperblock
 my_kernel[blockspergrid, threadsperblock](data)
 
 # Print the result
+print(data)
+print('###################################')
+data = numpy.ones((16, 16))
+threadsperblock = (16, 16)
+blockspergrid_x = math.ceil(data.shape[0] / threadsperblock[0])
+blockspergrid_y = math.ceil(data.shape[1] / threadsperblock[1])
+blockspergrid = (blockspergrid_x, blockspergrid_y)
+my_kernel_2D[blockspergrid, threadsperblock](data)
 print(data)
