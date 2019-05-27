9eb672837bd1ef2c7873386b13c3425a822bd517
ayas <ayas@aucegypt.edu>
2019-05-27 10:57:00 +0200

    TSNE Test

--- tfl_tools/ClusterFish.py
+++ tfl_tools/ClusterFish.py
@@ -104,7 +104,10 @@ for i in range(2):
     images_ = []
     for j in range(len(ind1)):
         images_.append(images[int(ind1[j])])
-    
+    ### added this because I have obtained index out of bounds
+    if ind1 > 1:
+        ind1 = ind1 - 1
+    ###
     tsne = TSNE(n_components=2, learning_rate=150, perplexity=30, angle=0.2, verbose=2).fit_transform(X[ind1,ind1])
 
     tx, ty = tsne[:,0], tsne[:,1]
