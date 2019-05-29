2dd01543d4a73f3d9077ac000676b3f08f334de9
ayas <ayas@aucegypt.edu>
2019-05-29 12:27:23 +0200

    writing to hdf5 files for better memory utilization

--- tfl_tools/split_dataset.py
+++ tfl_tools/split_dataset.py
@@ -66,7 +66,7 @@ def split_CV(n_splits = 10, save_split = True):
             np.savetxt(train_file, train, delimiter=' ', fmt='%s')
 
 def build_hd5(test_file, train_file, round = ''):
-    test_filename = image_set_test + str(input_width) + round + WIN + '.h5'
+    test_filename = str(image_set_test) + str(input_width) + round + str(WIN) + '.h5'
     print('Building hdf5 for the test set... ', test_filename)
     '''
     out_test_hd5 = os.path.join(DATABASE_PATH, test_filename)
