c6230634f95b7a0ec41c09f9b9bac22fa64240db
ayas <ayas@aucegypt.edu>
2019-06-10 16:28:44 +0200

    training on multiple gpu

--- tfl_tools/train_evaluate_AlexNet4.py
+++ tfl_tools/train_evaluate_AlexNet4.py
@@ -130,11 +130,13 @@ with tf.Graph().as_default(), tf.device('/cpu:0'):
                     # all towers.
                     model, conv_arr = VGGNet.build_model(model_file)
                     #loss = mg.tower_loss(scope, model, label_batch)
-                    print("start training round ", round_num)
-                    VGGNet.train(model, image_batch, label_batch, testX, testY, round_num, n_epoch, batch_size)
-
                     # Reuse variables for the next tower.
                     tf.get_variable_scope().reuse_variables()
+
+    print("start training round ", round_num)
+    VGGNet.train(model, image_batch, label_batch, testX, testY, round_num, n_epoch, batch_size)
+
+
 
 
 # Save
